d9a816cdf6d2eef48b648d0cd5a3782e2cd34763
Carmen Lopez Jurado <ca8625lo-s@student.lu.se>
2018-07-10 20:26:03 +0200

    Add files via upload

--- experimental_results.py
+++ experimental_results.py
@@ -5,33 +5,74 @@ Created on Sat May  5 21:40:19 2018
 @author: Carmen
 """
 from matplotlib.pyplot import *
-from numpy import *
-from pandas import *
+from numpy import linspace
+from pandas import read_csv
 import os
-
-data = read_csv(os.path.join('90_0_1st', 'r_data.csv'), header=None)
-
-x_dots = data.iloc[:,0]
-y_dots = data.iloc[:,1]
+import getopt, sys
 
 
-# x versus y pixel positions
-setInfo('$x$ [pixels]','$y$ [pixels]','First rod')
-gca().invert_yaxis()
-plot(x_dots, y_dots, 'o')
-show()
+def plot_data(rod):
+    number_frames = len(x_dots)
+    title = None
+    
+    if rod == '1':
+        title = 'First rod'
+    elif rod == '2':
+        title = 'Second rod'
+    
+    # x versus y pixel positions
+    setInfo('$x$ [pixels]','$y$ [pixels]',title)
+    gca().invert_yaxis()
+    plot(x_dots, y_dots, 'o')
+    show()
+    
+    #frames versus x
+    setInfo('frames ','$x$ [pixels]',title)
+    plot(linspace(1,number_frames,number_frames), x_dots, 'o')
+    show()
+    
+    #frames versus y
+    setInfo('frames ','$y$ [pixels]',title)
+    gca().invert_yaxis()
+    plot(linspace(1,number_frames,number_frames), y_dots, 'o')
+    show()
 
 
-frames = range(1, 900+1)
-print(len(frames))
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "f:d:")
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print('experimental_results.py -f <inputfile> -d <inputdata [inputdata2...]>')  
+        sys.exit(2)
+    
+    data_folder = None
+    data_files = None
+    
+    for o, a in opts:
+        if o == "-f":
+            data_folder = a
+        elif o == "-v":
+            data_files = a #mirar bien esto!!!
+        else:
+            assert False, "unhandled option"
 
-#frames versus x
-setInfo('frames ','$x$ [pixels]','First rod')
-plot(linspace(1,900,900), x_dots, 'o')
-show()
+    #me imagino que data_files es una lista
 
-#frames versus y
-setInfo('frames ','$y$ [pixels]','First rod')
-gca().invert_yaxis()
-plot(linspace(1,900,900), y_dots, 'o')
-show()
+    x_dots = None
+    y_dots = None
+    
+    for data_file in data_files:
+        data = read_csv(os.path.join(data_folder, data_file), header=None)
+    
+        x_dots = data.iloc[:,0]
+        y_dots = data.iloc[:,1]
+        
+        if '1' in data_file:
+            plot_data('1')
+        elif '2' in data_file:
+            plot_data('2')
+    
+
+if __name__ == "__main__":
+    main()
