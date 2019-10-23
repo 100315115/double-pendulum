4c777d6f3da813fac53a84370bfd8537a672e3c7
Carmen Lopez Jurado <ca8625lo-s@student.lu.se>
2019-10-23 23:31:41 +0200

    Add files via upload

--- theoretical_results.py
+++ theoretical_results.py
@@ -4,26 +4,11 @@ Created on Fri Apr 27 05:12:14 2018
 
 @author: Carmen
 """
+import sys
 from scipy.integrate import odeint
-from matplotlib.pyplot import plot, show
+from matplotlib.pyplot import plot, figure, show
 from numpy import sin, cos, pi, linspace, array
-
-def vectorfield(c, t, p):
-    theta_1, theta_2, w_1, w_2 = c
-    
-    g, m, l_1, l_2 = p
-    
-    a = ((g/l_1)*sin(theta_2)) - (w_1**2 * sin(theta_1 - theta_2))
-    b = ((m + 1)*(g/l_2)*sin(theta_1)) + (w_2**2 * sin(theta_1 - theta_2))
-    c = cos(theta_1 - theta_2)
-    d = m + (sin(theta_1 - theta_2))**2
-    
-    f = [w_1, 
-         w_2, 
-        ((c*a)-((l_2/l_1)*b))/d, 
-        ((c*b)-((m+1)*(l_1/l_2)*a))/d] 
-    
-    return f
+from useful_functions import *
 
 #Parameter values: gravity, first mass, second mass, lenght of the first rod 
 #and lenght of the second rod
@@ -48,63 +33,84 @@ p = [g,m_1/m_2,l_1,l_2]
 #Initial conditions
 c0 = [theta_1,theta_2,w_1,w_2]
 
-#For each second specified by the numpy array time, gets the angular velocity 
-#and angular displacement of both rods 
-sol = odeint(vectorfield, c0, time, args=(p,))
 
-#Angular displacements of both rods
-theta_1_array = array([i[0] for i in sol]) 
-theta_2_array = array([i[1] for i in sol])
+def vectorfield(c, t, p):
+    theta_1, theta_2, w_1, w_2 = c
+    
+    g, m, l_1, l_2 = p
+    
+    a = ((g/l_1) * sin(theta_2)) - (w_1**2 * sin(theta_1 - theta_2))
+    b = ((m + 1) * (g/l_2) * sin(theta_1)) + (w_2**2 * sin(theta_1 - theta_2))
+    c = cos(theta_1 - theta_2)
+    d = m + (sin(theta_1 - theta_2))**2
+    
+    f = [w_1, 
+         w_2, 
+        ((c * a)-((l_2 / l_1) * b)) / d, 
+        ((c * b) - ((m+1) * (l_1/l_2) * a)) / d] 
+    
+    return f
 
-
-#Theoretical data plots:
-
-#setInfo('time [s]','$\theta_1$ [rad]','First rod')
-#plot(time,theta_1_array)
-#show()
-
-
-#setInfo('time [s]','$\theta_2$ [rad]','Second rod')
-#plot(time,theta_2_array)
-#show()
-
-
-#First-rod plots---------------------------------------------------------------
-x = l_1*sin(theta_1_array) #x positions of the 1st mass
-y = l_1*-1*cos(theta_1_array) #y positions of the 1st mass
-
-#x vs y 
-setInfo('$x$ [m]','$y$ [m]','First rod')
-plot(x,y)
-show()
-
-#time vs x
-setInfo('time [s]','$x$ [m]','First rod')
-plot(time,x)
-show()
-
-#time vs y
-setInfo('time [s]','$y$ [m]','First rod')
-plot(time,y)
-show()
-
-
-
-#Second-rod plots--------------------------------------------------------------
-x = (l_1*sin(theta_1_array))+(l_2*sin(theta_2_array))#x positions 2nd mass
-y = (l_1*-1*cos(theta_1_array))+(l_2*-1*cos(theta_2_array))#y positions 2nd mass
-
-#x vs y
-setInfo('$x$ [m]','$y$ [m]','Second rod')
-plot(x,y)
-show()
-
-#time vs x
-setInfo('$time$ [s]','$x$ [m]','Second rod')
-plot(time,x)
-show()
-
-#time vs y
-setInfo('$time$ [s]','$y$ [m]','Second rod')
-plot(time,y)
-show()
+def main(prog_args):
+    
+    #For each second in the numpy array time, gets the angular velocity 
+    #and angular displacement of both rods 
+    sol = odeint(vectorfield, c0, time, args=(p,))
+    
+    #Angular displacements of both rods
+    theta_1_array = array([i[0] for i in sol]) 
+    theta_2_array = array([i[1] for i in sol])
+    
+    ###########################################################################
+    #                       FIRST-ROD PLOTS
+    ###########################################################################
+    x = l_1 * sin(theta_1_array) #x coordinates of the 1st mass
+    y = l_1 * -1 * cos(theta_1_array) #y coordinates of the 1st mass
+    
+    #x vs y 
+    figure('First rod x vs y')
+    plot_info('$x$ [m]','$y$ [m]','First rod')
+    plot(x,y)
+    show()
+    
+    #time vs x
+    figure('First rod time vs x')
+    plot_info('time [s]','$x$ [m]','First rod')
+    plot(time,x)
+    show()
+    
+    #time vs y
+    figure('First rod time vs y')
+    plot_info('time [s]','$y$ [m]','First rod')
+    plot(time,y)
+    show()
+    
+    
+    ###########################################################################
+    #                       SECOND-ROD PLOTS
+    ###########################################################################
+    x = (l_1 * sin(theta_1_array))+(l_2 * sin(theta_2_array))#x coordinates 2nd mass
+    y = (l_1 * -1 * cos(theta_1_array)) + (l_2 * -1 * cos(theta_2_array))#y coordinates 2nd mass
+    
+    #x vs y
+    figure('Second rod x vs y')
+    plot_info('$x$ [m]','$y$ [m]','Second rod')
+    plot(x,y)
+    show()
+    
+    #time vs x
+    figure('Second rod time vs x')
+    plot_info('$time$ [s]','$x$ [m]','Second rod')
+    plot(time,x)
+    show()
+    
+    #time vs y
+    figure('Second rod time vs y')
+    plot_info('$time$ [s]','$y$ [m]','Second rod')
+    plot(time,y)
+    show()
+    
+    pass
+    
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
---
+++ useful_functions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 21:37:24 2019
+
+@author: Carmen
+"""
+from matplotlib.pyplot import *
+from numpy import *
+
+def plot_info(xl,yl,t):
+    xlabel(xl)
+    ylabel(yl)
+    title(t)
